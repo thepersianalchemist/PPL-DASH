9237759f74ebf772fbe13f0a975bcb2d4aa92a4f
Aryan <aryan@skyaccess.com>
2026-04-22 21:31:56 -0700

    rewrite GL1/GL2/GL3 from full Fireflies transcripts (v4 seed)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // PPL Dashboard Service Worker
 // Bump CACHE_VERSION whenever you ship a new dashboard build.
-const CACHE_VERSION = 'ppl-dashboard-v6';
+const CACHE_VERSION = 'ppl-dashboard-v7';
 const CORE_ASSETS = [
   './',
   './index.html',
